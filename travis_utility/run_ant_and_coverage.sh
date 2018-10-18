c0d4872f3c9b54fce0fd7dc5639b9b04f3b7c066
Daniel <db123@bergqvist.se>
2018-10-18 17:55:03 +0200

    Update travis scripts

--- travis_utility/run_ant_and_coverage.sh
+++ travis_utility/run_ant_and_coverage.sh
@@ -4,21 +4,8 @@
 set -e
 
 
-pwd
-ls -al
-
-pwd
-ls -al lib
-
-
-
-
-
 # "ant test" compiles the project and runs the tests.
 # travis_utility/push-javadoc-to-docs.sh generates the javadoc and publish it.
 
 ant test && \
     travis_utility/push-javadoc-to-docs.sh
-
-# exit 4
-# exit 0
