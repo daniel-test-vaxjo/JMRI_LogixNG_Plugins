c607485bc287d48d2a9fa1ec1ee01fe0aa1c8180
Daniel <db123@bergqvist.se>
2018-10-18 23:55:03 +0200

    Update jmri.jar and Configurator.java

--- src/se/bergqvist/jmri_newlogix_plugin/Configurator.java
+++ src/se/bergqvist/jmri_newlogix_plugin/Configurator.java
@@ -1,23 +1,30 @@
 package se.bergqvist.jmri_newlogix_plugin;
 
+import java.util.Map;
 import javax.swing.JPanel;
 import jmri.jmrit.newlogix.swing.PluginConfiguratorInterface;
 
 /**
- * A configurator for the plugins in this class.
+ * A configurator for the plugins in this project.
  * 
  * @author Daniel Bergqvist Copyright 2018
  */
 public class Configurator implements PluginConfiguratorInterface {
 
     @Override
-    public JPanel getConfigPanel() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public JPanel getConfigPanel(String className) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void updateConfigFromPanel(JPanel pnl) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public JPanel getConfigPanel(String className, Map<String, String> config)
+            throws IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Map<String, String> getConfigFromPanel(JPanel panel) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }
